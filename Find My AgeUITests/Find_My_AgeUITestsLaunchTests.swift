d0876af341770aa03dcf2cd4fa5f78eb73fd4c2d
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2022-12-06 22:02:04 +0700

    Initial Commit

---
+++ Find My AgeUITests/Find_My_AgeUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Find_My_AgeUITestsLaunchTests.swift
+//  Find My AgeUITests
+//
+//  Created by Rifqi Muhammad Aziz on 06/12/22.
+//
+
+import XCTest
+
+final class Find_My_AgeUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
